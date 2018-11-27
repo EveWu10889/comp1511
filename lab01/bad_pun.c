5f565ef0763bd2c6b979723f8e3c88b8400fc973
EveWu10889 <43110652+EveWu10889@users.noreply.github.com>
2018-11-27 11:14:22 +1100

    Add files via upload

---
+++ lab01/bad_pun.c
@@ -0,0 +1,10 @@
+//A simple C program that belongs to Yuanyuan Wu
+//data 27/7/2018
+//by Yuanyuan Wu
+//for COMP1511 Lab01 Exercise 1
+
+#include <stdio.h>
+int main(void) {
+    printf("Hello, it is good to C you!\n");
+    return 0;
+}
---
+++ lab01/bird.c
@@ -0,0 +1,13 @@
+//A simple C program that belongs to Yuanyuan Wu
+//data 27/7/2018
+//by Yuanyuan Wu
+//for COMP1511 Lab01 Exercise 2
+
+#include <stdio.h>
+int main(void) {
+    printf("  ___\n");
+    printf(" ('v')\n");
+    printf("((___))\n");
+    printf(" ^   ^\n");
+    return 0;
+}
